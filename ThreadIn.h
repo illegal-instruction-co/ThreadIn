a8767a8723663900bf0d8f9700844846c718b3a1
illegal-instruction-co <machinetherapist2@gmail.com>
2023-06-09 03:14:34 +0300

    make it able to work externally as well

--- ThreadIn.h
+++ ThreadIn.h
@@ -11,81 +11,90 @@
 #include <TlHelp32.h>
 
 namespace ii {
-	class ThreadIn final {
+class ThreadIn final {
 
-	public:
-		ThreadIn(__int64 threadStartAddress)
-			: m_currentProcessId(GetCurrentProcessId()), m_thread([this, threadStartAddress]() { Freeze(threadStartAddress, 0); }) {}
+public:
+	ThreadIn(__int64 threadStartAddress)
+		: m_currentProcessId(GetCurrentProcessId()), m_thread([this, threadStartAddress]() { Freeze(threadStartAddress, 0); }) {}
 
-		ThreadIn(__int64 threadStartAddress, __int64 forwardAddress)
-			: m_currentProcessId(GetCurrentProcessId()), m_thread([this, threadStartAddress, forwardAddress]() {
-			while (true) {
-				Freeze(threadStartAddress, forwardAddress);
-			}
-				}) {}
+	ThreadIn(__int64 threadStartAddress, __int64 forwardAddress)
+		: m_currentProcessId(GetCurrentProcessId()), m_thread([this, threadStartAddress, forwardAddress]() {
+			  while (true) {
+				  Freeze(threadStartAddress, forwardAddress);
+			  }
+		  }) {}
 
-	private:
-		__int64 m_currentProcessId;
+	ThreadIn(DWORD pid, __int64 threadStartAddress) : m_currentProcessId(pid), m_thread([this, threadStartAddress]() { Freeze(threadStartAddress, 0); }) {}
 
-		std::jthread m_thread;
+	ThreadIn(DWORD pid, __int64 threadStartAddress, __int64 forwardAddress)
+		: m_currentProcessId(pid), m_thread([this, threadStartAddress, forwardAddress]() {
+			  while (true) {
+				  Freeze(threadStartAddress, forwardAddress);
+			  }
+		  }) {}
 
-		enum THREADINFOCLASS {
-			ThreadQuerySetWin32StartAddress = 9,
-		};
+private:
+	__int64 m_currentProcessId;
 
-		typedef DWORD(__stdcall* f_NtQueryInformationThread)(HANDLE, THREADINFOCLASS, void*, ULONG_PTR, ULONG_PTR*);
+	std::jthread m_thread;
 
-		ULONG_PTR GetThreadStartAddress(HANDLE hThread) {
-			auto NtQueryInformationThread =
-				reinterpret_cast<f_NtQueryInformationThread>(GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtQueryInformationThread"));
-			if (!NtQueryInformationThread)
-				return 0;
+	enum THREADINFOCLASS {
+		ThreadQuerySetWin32StartAddress = 9,
+	};
 
-			ULONG_PTR ulStartAddress = 0;
-			DWORD ret = NtQueryInformationThread(hThread, ThreadQuerySetWin32StartAddress, &ulStartAddress, sizeof(ULONG_PTR), nullptr);
+	typedef DWORD(__stdcall* f_NtQueryInformationThread)(HANDLE, THREADINFOCLASS, void*, ULONG_PTR, ULONG_PTR*);
 
-			if (ret)
-				return 0;
+	ULONG_PTR GetThreadStartAddress(HANDLE hThread) {
+		auto NtQueryInformationThread =
+			reinterpret_cast<f_NtQueryInformationThread>(GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtQueryInformationThread"));
+		if (!NtQueryInformationThread)
+			return 0;
 
-			return ulStartAddress;
-		}
+		ULONG_PTR ulStartAddress = 0;
+		DWORD ret = NtQueryInformationThread(hThread, ThreadQuerySetWin32StartAddress, &ulStartAddress, sizeof(ULONG_PTR), nullptr);
 
-		void Freeze(__int64 tSA, __int64 forwardAddress) {
+		if (ret)
+			return 0;
 
-			HANDLE hThreadSnap = INVALID_HANDLE_VALUE;
-			THREADENTRY32 te32;
-			CONTEXT ctx;
+		return ulStartAddress;
+	}
 
-			hThreadSnap = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
-			if (hThreadSnap == INVALID_HANDLE_VALUE)
-				return;
+	void Freeze(__int64 tSA, __int64 forwardAddress) {
 
-			te32.dwSize = sizeof(te32);
-			ctx.ContextFlags = CONTEXT_FULL;
+		HANDLE hThreadSnap = INVALID_HANDLE_VALUE;
+		THREADENTRY32 te32;
+		CONTEXT ctx;
 
-			Thread32First(hThreadSnap, &te32);
-			while (Thread32Next(hThreadSnap, &te32)) {
-				if (te32.th32OwnerProcessID == m_currentProcessId) {
-					HANDLE hThread = OpenThread(THREAD_ALL_ACCESS, FALSE, te32.th32ThreadID);
+		hThreadSnap = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+		if (hThreadSnap == INVALID_HANDLE_VALUE)
+			return;
 
-					if (GetThreadStartAddress(hThread) == tSA || tSA == 0) {
-						GetThreadContext(hThread, &ctx);
+		te32.dwSize = sizeof(te32);
+		ctx.ContextFlags = CONTEXT_FULL;
 
-						if (forwardAddress == 0) {
-							ctx.XIP = (__int64)+[]() noexcept {
-								if (__rdtsc())
-									WaitMessage();
-							};
-						}
-						else
-							ctx.XIP = forwardAddress;
+		Thread32First(hThreadSnap, &te32);
+		while (Thread32Next(hThreadSnap, &te32)) {
+			if (te32.th32OwnerProcessID == m_currentProcessId) {
+				HANDLE hThread = OpenThread(THREAD_ALL_ACCESS, FALSE, te32.th32ThreadID);
 
-						SetThreadContext(hThread, &ctx);
+				if (GetThreadStartAddress(hThread) == tSA || tSA == 0) {
+					GetThreadContext(hThread, &ctx);
+
+					if (forwardAddress == 0) {
+						ctx.XIP = (__int64)+[]() noexcept {
+							if (__rdtsc())
+								WaitMessage();
+						};
 					}
+					else
+						ctx.XIP = forwardAddress;
+
+					SetThreadContext(hThread, &ctx);
 				}
 			}
-
-			CloseHandle(hThreadSnap);
 		}
-	};
+
+		CloseHandle(hThreadSnap);
+	}
+};
 } // namespace ii
